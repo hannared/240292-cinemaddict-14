e0b525b9a1ef33a14be77042dcbe356c282eaa3a
Anna K <hanna.korsak@gmail.com>
2021-04-09 00:13:03 +0200

    add sort by default and rating

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import { createFilmMostCommentedTemplate } from './view/film-most-commented.js';
 import { createUserProfileTemplate } from './view/user-profile.js';
 import { createFilmDetailsTemplate } from './view/film-details.js';
 import { createShowMoreBtnTemplate } from './view/show-more-btn.js';
-import { createSortingTemplate, getSortByDateFilms } from './view/sorting.js';
+import {
+  createSortingTemplate,
+  getSortByDateFilms,
+  getSortByDefaultFilms,
+  getSortByRatingFilms,
+} from './view/sorting.js';
 import { createFooterStatisticsTemplate } from './view/footer-statistics.js';
 import { generateFilms } from './mock/film.js';
 
@@ -14,7 +19,7 @@ const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
 const siteFooterElement = document.querySelector('.footer__statistics');
 
-const films = getSortByDateFilms(generateFilms());
+const films = getSortByDefaultFilms(generateFilms());
 console.log(films);
 
 const render = (container, template, place = 'beforeend') => {
