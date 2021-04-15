61f72596844dafb3b09c5e93f242a4d31f8a7e02
Anna K <hanna.korsak@gmail.com>
2021-04-15 23:18:57 +0200

    fix eslint

--- src/view/abstract.js
+++ src/view/abstract.js
@@ -3,7 +3,7 @@ import { createElement } from '../utils';
 export default class Abstract {
   constructor() {
     if (new.target === Abstract) {
-      throw new Error("Can't instantiate Abstract, only concrete one.");
+      throw new Error('Can not instantiate Abstract, only concrete one.');
     }
 
     this._element = null;
