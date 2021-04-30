24dcd07f65923ed212a4f771d2dd2a4ad6c6329e
Anna K <hanna.korsak@gmail.com>
2021-04-30 11:24:28 +0200

    add smart class

---
+++ src/view/smart.js
@@ -0,0 +1,34 @@
+import Abstract from './abstract';
+
+export default class Smart extends Abstract {
+  constructor() {
+    super();
+  }
+  restoreHandlers() {
+    //абстрактный метод restoreHandlers, его нужно будет реализовать в наследнике.
+    //Его задача — восстанавливать обработчики событий после перерисовки;
+  }
+
+  updateElement() {
+    const prevElement = this.getElement();
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parent.replaceChild(newElement, prevElement);
+  }
+  //удалить старый DOM-элемент компонента;
+  // создать новый DOM-элемент;
+  // поместить новый элемент вместо старого;
+
+  updateData(update) {
+    if (!update) {
+      return;
+    }
+
+    this._data = Object.assign({}, this._data, update);
+
+    this.updateElement();
+  }
+}
