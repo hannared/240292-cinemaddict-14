94522dd79656b635da2931823f68395747d9ebc0
Anna K <hanna.korsak@gmail.com>
2021-05-17 15:32:04 +0200

    add stats btns wip

--- src/presenter/stats-presenter.js
+++ src/presenter/stats-presenter.js
@@ -1,6 +1,7 @@
 import { renderElement } from '../utils';
 import { ChartView } from '../view/chart-view';
 import Stats from '../view/stats';
+import StatsFilterButton, { StatsFilterLabel } from '../view/stats-filter-btn';
 
 export default class Statistics {
   constructor(homeContainer, movies, statsFilters) {
@@ -29,12 +30,114 @@ export default class Statistics {
     this._stats.showElement();
   }
 
+  _renderStatsFilters() {
+    const filterAll = new StatsFilterButton('all-time', 'all-time', true);
+    const filterAllLabel = new StatsFilterLabel('all-time', 'All time');
+
+    const filterToday = new StatsFilterButton('today', 'today', false);
+    const filterTodayLabel = new StatsFilterLabel('today', 'Today');
+
+    const filterWeek = new StatsFilterButton('week', 'week', false);
+    const filterWeekLabel = new StatsFilterLabel('week', 'Week');
+
+    const filterMonth = new StatsFilterButton('month', 'month', false);
+    const filterMonthLabel = new StatsFilterLabel('month', 'Month');
+
+    const filterYear = new StatsFilterButton('year', 'year', false);
+    const filterYearLabel = new StatsFilterLabel('year', 'Year');
+
+    renderElement(
+      this._stats.getElement().querySelector('.statistic__filters'),
+      filterAll,
+    );
+    renderElement(
+      this._stats.getElement().querySelector('.statistic__filters'),
+      filterAllLabel,
+    );
+
+    filterAll.setStatsFilterClickHandler(() => {
+      filterToday.reset();
+      filterWeek.reset();
+      filterMonth.reset();
+      filterYear.reset();
+    });
+
+    renderElement(
+      this._stats.getElement().querySelector('.statistic__filters'),
+      filterToday,
+    );
+
+    renderElement(
+      this._stats.getElement().querySelector('.statistic__filters'),
+      filterTodayLabel,
+    );
+
+    filterToday.setStatsFilterClickHandler(() => {
+      filterAll.reset();
+      filterWeek.reset();
+      filterMonth.reset();
+      filterYear.reset();
+    });
+
+    renderElement(
+      this._stats.getElement().querySelector('.statistic__filters'),
+      filterWeek,
+    );
+
+    renderElement(
+      this._stats.getElement().querySelector('.statistic__filters'),
+      filterWeekLabel,
+    );
+
+    filterWeek.setStatsFilterClickHandler(() => {
+      filterAll.reset();
+      filterToday.reset();
+      filterMonth.reset();
+      filterYear.reset();
+    });
+
+    renderElement(
+      this._stats.getElement().querySelector('.statistic__filters'),
+      filterMonth,
+    );
+
+    renderElement(
+      this._stats.getElement().querySelector('.statistic__filters'),
+      filterMonthLabel,
+    );
+
+    filterMonth.setStatsFilterClickHandler(() => {
+      filterAll.reset();
+      filterToday.reset();
+      filterWeek.reset();
+      filterYear.reset();
+    });
+
+    renderElement(
+      this._stats.getElement().querySelector('.statistic__filters'),
+      filterYear,
+    );
+
+    renderElement(
+      this._stats.getElement().querySelector('.statistic__filters'),
+      filterYearLabel,
+    );
+    filterYear.setStatsFilterClickHandler(() => {
+      filterAll.reset();
+      filterToday.reset();
+      filterWeek.reset();
+      filterMonth.reset();
+    });
+  }
+
   _renderStats() {
     this._renderStatsInfo();
   }
 
   render() {
     this._renderStats();
+
+    this._renderStatsFilters();
 
     this.hide();
 
